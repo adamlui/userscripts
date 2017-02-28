0835e2bfaad2ed7937bccd12df482550befa1d35
Adam Lui <adam@mturkers.org>
2017-02-27 16:03:35 -0800

    Update highlight-radio-bubbles.js

--- highlight-radio-bubbles.js
+++ highlight-radio-bubbles.js
@@ -4,8 +4,10 @@
 // @version     2017.02.26
 // @descrption  Makes radio bubbles bigger, more colorful and thereby more conspicuous when brought to focus. Ideal for completing surveys and long HITs on MTurk.
 // @author      adaaaam
-// @icon        https://mturkers.org/images/04nrzj7K_400x400.png
 // @include     *
+// @icon        https://mturkers.org/images/04nrzj7K_400x400.png
+// @updateURL   https://raw.githubusercontent.com/adaaaam/userscripts/master/highlight-radio-bubbles.js
+// @downloadURL https://raw.githubusercontent.com/adaaaam/userscripts/master/highlight-radio-bubbles.js
 // ==/UserScript==
 
 var sheet = document.createElement('style') ;
