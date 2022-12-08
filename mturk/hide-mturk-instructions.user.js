5e99c0989ac75d32313bddb76a082e2b07161e8f
Adam Lui <adamlui@protonmail.com>
2022-12-08 08:05:16 -0800

    Added @icon

--- mturk/hide-mturk-instructions.user.js
+++ mturk/hide-mturk-instructions.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Hide MTurk Instructions
-// @version      2022.12.08
+// @version      2022.12.08.2
 // @author       Adam Lui
 // @namespace    https://elonsucks.org/@adam
 // @description  Hides instructions from HITs on MTurk
 // @match        https://*.mturkcontent.com/*
+// @icon         https://www.mturk.com/assets/images/favicon.ico
 // @run-at       document-start
 // @grant        GM_addStyle
 // @updateURL    https://greasyfork.org/scripts/28351-hide-mturk-instructions/code/hide-mturk-instructions.meta.js
