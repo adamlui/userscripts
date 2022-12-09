3ef9c080ded78c7c933df63f4d55773673a5c0e0
adamlui@protonmail.com <adamlui@protonmail.com>
2022-12-09 05:36:34 -0800

    Added metadata

--- mturk/hide-mturk-instructions.user.js
+++ mturk/hide-mturk-instructions.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Hide MTurk Instructions
-// @version      2022.12.08.2
+// @version      2022.12.09
 // @author       Adam Lui
 // @namespace    https://elonsucks.org/@adam
 // @description  Hides instructions from HITs on MTurk
-// @license      mit
-// @match        https://*.mturkcontent.com/*
+// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @icon         https://www.mturk.com/assets/images/favicon.ico
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   opera
+// @compatible   safari
+// @compatible   edge
+// @match        https://*.mturkcontent.com/*
 // @run-at       document-start
 // @grant        GM_addStyle
 // @updateURL    https://greasyfork.org/scripts/28351-hide-mturk-instructions/code/hide-mturk-instructions.meta.js
