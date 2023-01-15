d0b5bf357e7fdfedc108c4854eddabc1f901b77e
Adam Lui <adamlui@protonmail.com>
2023-01-15 14:30:34 -0800

    Removed jQuery dependency, drastically reduced code, added metadata

--- xenforo-vbulletin-media-killer.user.js
+++ xenforo-vbulletin-media-killer.user.js
@@ -1,28 +1,21 @@
 // ==UserScript==
-// @name        Hide Forum Images
-// @version     2022.12.07.4
-// @author      Adam Lui
-// @namespace   https://elonsucks.org/@adam
-// @description Hides images + videos from XenForo and vBulletin forums.
-// @include     *
-// @require     http://code.jquery.com/jquery-latest.min.js
+// @name             Hide Forum Images
+// @version          2023.01.15
+// @author           Adam Lui
+// @namespace        https://elonsucks.org/@adam
+// @description      Hides images/videos from XenForo and vBulletin forums.
+// @supportURL       https://github.com/adamlui/userscripts/issues
+// @license          MIT
+// @compatible       chrome
+// @compatible       firefox
+// @compatible       opera
+// @compatible       safari
+// @compatible       edge
+// @match            http*://*/*
+// @grant            GM_addStyle
+// @updateURL        https://greasyfork.org/en/scripts/12639/code/youtube-classic.meta.js
+// @downloadURL      https://greasyfork.org/en/scripts/12639/code/youtube-classic.user.js
 // ==/UserScript==
 
-if (document.querySelector('html[id^="vbulletin"]') || document.querySelector('html[id^="XenForo"]')) {
-    var imgs = document.getElementsByTagName('img');
-    for (var n = imgs.length; n--> 0;) {
-        var img = imgs[n];
-        img.setAttribute("src", "");
-    }
-    $(document).ready(function() {
-        var $vid_embed = $('iframe[src^="//www.youtube.com"],iframe[src*="gifv"]');
-        $vid_embed.each(function() {
-            var $this = $(this);
-            var vid_url = $this.attr('src');
-            var vid_id = vid_url.replace(/(.*)embed\/(.*)\?(.*)/, "$2");
-            var yt_url = "https://www.youtube.com/watch?v=" + vid_id;
-            $this.after('<br><a href="'+yt_url+'" target="_blank">'+yt_url+'</a>');
-            $this.remove();
-        });
-    });
-}
+if (document.querySelector('[src*="vbulletin"]') || document.querySelector('a[href*="xenforo"]')) {
+    GM_addStyle('img, [style*="background-image"], [class*="avatar"], [class*="player"] { display:none !important; }');}
