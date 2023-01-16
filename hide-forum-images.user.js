40ddfa3b1e6e781ea170c707a8973567f95eb25b
adamlui@protonmail.com <adamlui@protonmail.com>
2023-01-16 08:13:26 -0800

    Changed XenForo forum detection method
    + Fixed update & download URLs

--- hide-forum-images.user.js
+++ hide-forum-images.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             Hide Forum Images
-// @version          2023.01.15.1
+// @version          2023.01.16
 // @author           Adam Lui
 // @namespace        https://elonsucks.org/@adam
 // @description      Hides images/videos from XenForo and vBulletin forums.
@@ -13,10 +13,9 @@
 // @compatible       edge
 // @match            http*://*/*
 // @grant            GM_addStyle
-// @updateURL        https://greasyfork.org/en/scripts/12639/code/youtube-classic.meta.js
-// @downloadURL      https://greasyfork.org/en/scripts/12639/code/youtube-classic.user.js
+// @updateURL        https://greasyfork.org/scripts/12639-hide-forum-images/code/hide-forum-images.meta.js
+// @downloadURL      https://greasyfork.org/scripts/12639-hide-forum-images/code/hide-forum-images.user.js
 // ==/UserScript==
 
-if (document.querySelector('[src*="vbulletin"]') || document.querySelector('a[href*="xenforo"]')) {
-    GM_addStyle('img, [style*="background-image"], [class*="avatar"], [class*="player"] { display:none !important; }');
-}
+if (document.querySelector('[src*="vbulletin"]') || document.querySelector('.copyright').textContent.match(/xenforo/i)) {
+    GM_addStyle('img, [style*="background-image"], [class*="avatar"], [class*="player"] { display:none !important; }');}
