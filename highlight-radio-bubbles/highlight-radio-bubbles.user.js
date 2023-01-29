59e59cee12941764852f14fd0c184b5086f09b5b
Adam Lui <adamlui@protonmail.com>
2023-01-29 02:43:05 -0800

    Updated metadata

--- highlight-radio-bubbles/highlight-radio-bubbles.user.js
+++ highlight-radio-bubbles/highlight-radio-bubbles.user.js
@@ -1,15 +1,22 @@
 // ==UserScript==
-// @name        Highlight Radio Bubbles
-// @namespace   https://mturkers.org/adaaaam
-// @version     2017.02.26
-// @descrption  Makes radio bubbles bigger, more colorful and thereby more conspicuous when brought to focus. Ideal for completing surveys and long HITs on MTurk.
-// @author      adaaaam
-// @include     *
-// @icon        https://mturkers.org/images/04nrzj7K_400x400.png
-// @updateURL   https://raw.githubusercontent.com/adaaaam/userscripts/master/highlight-radio-bubbles.js
-// @downloadURL https://raw.githubusercontent.com/adaaaam/userscripts/master/highlight-radio-bubbles.js
+// @name             Highlight Radio Bubbles
+// @version          2023.01.29
+// @author           Adam Lui
+// @namespace        https://elonsucks.org/@adam
+// @description      Makes radio bubbles bigger & more colorful when brought to focus.
+// @supportURL       https://github.com/adamlui/userscripts/issues
+// @license          MIT
+// @icon             https://i.imgur.com/ribh0wE.png
+// @compatible       chrome
+// @compatible       firefox
+// @compatible       opera
+// @compatible       safari
+// @compatible       edge
+// @match            http*://*/*
+// @updateURL        https://greasyfork.org/scripts/26311/code/highlight-radio-bubbles.meta.js
+// @downloadURL      https://greasyfork.org/scripts/26311/code/highlight-radio-bubbles.user.js
 // ==/UserScript==
 
-var sheet = document.createElement('style') ;
-sheet.innerHTML = "input[type=radio]:focus { outline-color: red !important ; width: 25px !important ; height: 25px !important ; }" ;
-document.head.appendChild(sheet) ;
+var styleNode = document.createElement('style')
+styleNode.innerHTML = 'input[type=radio]:focus { outline-color: red !important ; width: 25px !important ; height: 25px !important ; }'
+document.head.appendChild(styleNode)
