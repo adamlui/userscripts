b155b29c1ce94b04332004bd0c89506fe43a714e
adamlui@protonmail.com <adamlui@protonmail.com>
2023-02-11 03:37:55 -0800

    Added Violentmonkey support

--- chatgpt/duckduckgpt/duckduckgpt.user.js
+++ chatgpt/duckduckgpt/duckduckgpt.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             DuckDuckGPT
-// @version          2023.01.29
+// @version          2023.01.29.1
 // @author           Adam Lui
 // @namespace        https://github.com/adamlui
 // @description      Adds ChatGPT answers to DuckDuckGo sidebar
@@ -10,21 +10,28 @@
 // @icon64           https://raw.githubusercontent.com/adamlui/userscripts/master/duckduckgpt/images/ddgpt-icon64.png
 // @compatible       chrome
 // @compatible       firefox
-// @compatible       opera
-// @compatible       safari
-// @compatible       edge
 // @match            https://duckduckgo.com/*
 // @connect          chat.openai.com
 // @grant            GM_deleteValue
 // @grant            GM_getValue
 // @grant            GM_setValue
+// @grant            GM_info
 // @grant            GM_xmlhttpRequest
 // ==/UserScript==
 
-var GM_setValue = (() => window.GM_setValue)();
-var GM_deleteValue = (() => window.GM_deleteValue)();
-var GM_xmlhttpRequest = (() => window.GM_xmlhttpRequest)();
-var GM_getValue = (() => window.GM_getValue)();
+var monkeyWindow = window
+var GM_setValue = (() => window.GM_setValue)()
+var GM_deleteValue = (() => window.GM_deleteValue)()
+var GM_info = (() => monkeyWindow.GM_info)()
+var GM_xmlhttpRequest = (() => window.GM_xmlhttpRequest)()
+var GM_getValue = (() => window.GM_getValue)()
+
+function getUserscriptManager() {
+    try {
+        const userscriptManager = GM_info.scriptHandler
+        return userscriptManager
+    } catch (error) { return "other" }
+}
 
 var alerts = {
     waitingResponse: "Waiting for ChatGPT response...",
@@ -36,68 +43,61 @@ var alerts = {
 };
 
 function uuidv4() {
-    var s = [];
-    var hexDigits = "0123456789abcdef";
+    var s = [], hexDigits = "0123456789abcdef"
     for (var i = 0; i < 36; i++) {
         s[i] = hexDigits.substr(Math.floor(Math.random() * 16), 1); }
-    s[14] = "4";
-    s[19] = hexDigits.substr(s[19] & 3 | 8, 1);
-    s[8] = s[13] = s[18] = s[23] = "-";
-    var uuid = s.join("");
-    return uuid;
+    s[14] = "4"
+    s[19] = hexDigits.substr(s[19] & 3 | 8, 1)
+    s[8] = s[13] = s[18] = s[23] = "-"
+    var uuid = s.join("")
+    return uuid
 }
 
 function i18n(name, param) {
-    return alerts[name] ? alerts[name].replace("#t#", param) : name;
-}
+    return alerts[name] ? alerts[name].replace("#t#", param) : name }
 
 var container = document.createElement("div")
 function getContainer() { return container }
 
 function containerShow(answer) {
-    var container2 = getContainer();
-    container2.innerHTML = '<p><span class="prefix">ChatGPT</span><pre></pre></p>';
-    container2.querySelector("pre").textContent = answer;
+    var container2 = getContainer()
+    container2.innerHTML = '<p><span class="prefix">ChatGPT</span><pre></pre></p>'
+    container2.querySelector("pre").textContent = answer
 }
 
 function containerAlert(htmlStr) {
-    var container2 = getContainer();
-    container2.innerHTML = htmlStr;
+    var container2 = getContainer()
+    container2.innerHTML = htmlStr
 }
 
 function alertLogin() {
-    containerAlert(`<p>${i18n("login")}<a href="https://chat.openai.com" target="_blank" rel="noreferrer">chat.openai.com</a></p>`);
-}
+    containerAlert(`<p>${i18n("login")}<a href="https://chat.openai.com" target="_blank" rel="noreferrer">chat.openai.com</a></p>`) }
 
 function alertBlockedByCloudflare() {
-    containerAlert(`<p>${i18n("checkCloudflare")}<a href="https://chat.openai.com" target="_blank" rel="noreferrer">chat.openai.com</a></p>`);
-}
+    containerAlert(`<p>${i18n("checkCloudflare")}<a href="https://chat.openai.com" target="_blank" rel="noreferrer">chat.openai.com</a></p>`) }
 
 function alertFrequentRequests() {
-    containerAlert(`<p>${i18n("tooManyRequests")}</p>`);
-}
+    containerAlert(`<p>${i18n("tooManyRequests")}</p>`) }
 
 function isBlockedbyCloudflare(resp) {
     try {
-        var html = new DOMParser().parseFromString(resp, "text/html");
-        var title = html.querySelector("title");
-        return title.innerText === "Just a moment...";
-    } catch (error) {
-        return false;
-    }
+        var html = new DOMParser().parseFromString(resp, "text/html")
+        var title = html.querySelector("title")
+        return title.innerText === "Just a moment..."
+    } catch (error) { return false }
 }
 
 async function getAnswer(question, callback) {
     try {
-        var accessToken = await getAccessToken();
+        var accessToken = await getAccessToken()
         GM_xmlhttpRequest({
             method: "POST",
             url: "https://chat.openai.com/backend-api/conversation",
             headers: {
-                "Content-Type": "	application/json",
+                "Content-Type": "   application/json",
                 Authorization: `Bearer ${accessToken}`
             },
-            responseType: "stream",
+            responseType: responseType(),
             data: JSON.stringify({
                 action: "next",
                 messages: [
@@ -113,88 +113,73 @@ async function getAnswer(question, callback) {
                 model: "text-davinci-002-render",
                 parent_message_id: uuidv4()
             }),
-            onloadstart: onloadstart(),
-            onload: onload()
-        });
+            onloadstart: onLoadStart(),
+            onload: onLoad()
+        })
     } catch (error) {
         if (error === "UNAUTHORIZED") {
-            GM_deleteValue("accessToken");
-            alertLogin();
+            GM_deleteValue("accessToken")
+            alertLogin()
         }
-        console.error("getAnswer error: ", error);
+        console.error("getAnswer error: ", error)
     }
-    function onload() {
-        function finish() {
-            if (typeof callback === "function") {
-                return callback("finish");
-            }
-        }
-        finish();
+    function responseType() {
+      if (getUserscriptManager() === "Tampermonkey") {
+        return "stream" } else { return "text" }
+    }
+    function onLoad() {
         return function(event) {
             if (event.status === 401) { GM_deleteValue("accessToken") ; alertLogin() }
             if (event.status === 403) { alertBlockedByCloudflare() }
             if (event.status === 429) { alertFrequentRequests() }
-        };
+            if (getUserscriptManager() !== "Tampermonkey") {
+                if (event.response) {
+                    const answer = JSON.parse(event.response
+                        .split("\n\n").slice(-3, -2)[0].slice(6)).message.content.parts[0]
+                    containerShow(answer)
+        }}}
     }
-    function onloadstart() {
-        return function(stream) {
-            var reader = stream.response.getReader();
-            reader.read().then(function processText({ done, value }) {
-                if (done) {
-                    return;
-                }
-                let responseItem = String.fromCharCode(...Array.from(value));
-                var items = responseItem.split("\n\n");
-                if (items.length > 2) {
-                    var lastItem = items.slice(-3, -2)[0];
-                    if (lastItem.startsWith("data: [DONE]")) {
-                        responseItem = items.slice(-4, -3)[0];
-                    } else {
-                        responseItem = lastItem;
+    function onLoadStart() {
+        if (getUserscriptManager() === "Tampermonkey") {
+            return function(stream) {
+                var reader = stream.response.getReader()
+                reader.read().then(function processText({ done, value }) {
+                    if (done) { return }
+                    let responseItem = String.fromCharCode(...Array.from(value))
+                    var items = responseItem.split("\n\n")
+                    if (items.length > 2) {
+                        var lastItem = items.slice(-3, -2)[0]
+                        if (lastItem.startsWith("data: [DONE]")) {
+                            responseItem = items.slice(-4, -3)[0]
+                        } else { responseItem = lastItem }
                     }
-                }
-                if (responseItem.startsWith("data: {")) {
-                    var answer = JSON.parse(responseItem.slice(6)).message.content.parts[0];
-                    containerShow(answer);
-                } else if (responseItem.startsWith("data: [DONE]")) {
-                    return;
-                }
-                return reader.read().then(processText);
-            });
-        };
-    }
+                    if (responseItem.startsWith("data: {")) {
+                        var answer = JSON.parse(responseItem.slice(6)).message.content.parts[0]
+                        containerShow(answer)
+                    } else if (responseItem.startsWith("data: [DONE]")) { return }
+                    return reader.read().then(processText)
+    })}}}
 }
 
 function getAccessToken() {
     return new Promise(async (resolve, rejcet) => {
-        var accessToken = await GM_getValue("accessToken");
+        var accessToken = await GM_getValue("accessToken")
         if (!accessToken) {
             GM_xmlhttpRequest({
                 url: "https://chat.openai.com/api/auth/session",
                 onload: function(response) {
                     if (isBlockedbyCloudflare(response.responseText)) {
-                        alertLogin();
-                        return;
-                    }
+                        alertLogin() ; return }
                     var accessToken2 = JSON.parse(response.responseText).accessToken;
-                    if (!accessToken2) {
-                        rejcet("UNAUTHORIZED");
-                    }
-                    GM_setValue("accessToken", accessToken2);
-                    resolve(accessToken2);
+                    if (!accessToken2) { rejcet("UNAUTHORIZED") }
+                    GM_setValue("accessToken", accessToken2)
+                    resolve(accessToken2)
                 },
-                onerror: function(error) {
-                    rejcet(error);
-                },
-                ontimeout: () => {
-                    console.error("getAccessToken timeout!");
-                }
-            });
-        } else {
-            resolve(accessToken);
-        }
-    });
-}
+                onerror: function(error) { rejcet(error) },
+                ontimeout: () => { console.error("getAccessToken timeout!") }
+            })
+        } else { resolve(accessToken) }
+})}
 
 // Stylize ChatGPT container to match DDG's
 var css = `
