2d1cebf68f2f1a3c0bd8f6f7f66308586cf87af4
Adam Lui <adamlui@protonmail.com>
2023-03-09 07:31:25 -0800

    Implemented tooltips, moved mode styling...
    ...out of `toggleMode()` to top of script for easier access
    + Renamed variables (such as `virginToggled` to `iniStateChecked` to un-🍆)
    - Removed `isModeON()`

--- chatgpt/chatgpt-widescreen-mode/chatgpt-widescreen-mode.user.js
+++ chatgpt/chatgpt-widescreen-mode/chatgpt-widescreen-mode.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             ChatGPT Widescreen Mode 🖥️
-// @version          2023.03.09.1
+// @version          2023.03.09.2
 // @author           Adam Lui & Xiao Ying Yo
 // @namespace        https://github.com/adamlui
 // @namespace        https://github.com/xiaoyingyo
@@ -18,65 +18,97 @@
 
 (function() {
 
-    // Collect OpenAI classes + input button color
-    var sendButtonClasses = document.querySelector("form button[class*='bottom']").classList
-    var sendSVGclasses = document.querySelector("form button[class*='bottom'] > svg").classList
-    var inputButtonColor = 'currentColor' // value changes w/ scheme
-    var sidepadClasses = classListToCSS(
-        document.querySelector('#__next > div > div').classList )
-    var sidebarClasses = classListToCSS(
-        document.querySelector('#__next > div > div.dark').classList )
+    var tooltips = {
+        wideScreenON: 'Exit wide screen', wideScreenOFF: 'Wide screen',
+        fullWindowON: 'Exit full window', fullWindowOFF: 'Full-window mode',
+        sendButton: 'Send message'
+    }
+
+    // Create/stylize tooltip div
+    var tooltipDiv = document.createElement('div')
+    tooltipDiv.classList.add('toggle-tooltip')
+    var tooltipStyle = document.createElement('style')
+    tooltipStyle.innerHTML = `
+        .toggle-tooltip {
+            /* Box style */   background: black ; padding: 5px ; border-radius: 5px ;
+            /* Font style */  font-size: 0.7rem ; color: white ;
+            /* Visibility */  position: absolute ; top: -22px ; opacity: 0 ; transition: opacity 0.1s ; z-index: 9999 }`
+    document.head.appendChild(tooltipStyle)
+
+    // Collect OpenAI classes/colors
+    var sendButton = document.querySelector('form button[class*="bottom"]')
+    var sendButtonColor = 'currentColor' // changes w/ scheme
+    var sendButtonClasses = document.querySelector('form button[class*="bottom"]').classList
+    var sendSVGclasses = document.querySelector('form button[class*="bottom"] > svg').classList
+    var sidepadClasses = classListToCSS( document.querySelector('#__next > div > div').classList )
+    var sidebarClasses = classListToCSS( document.querySelector('#__next > div > div.dark').classList )
+
+    // Create wide screen style
+    var wideScreenStyle = document.createElement('style')
+    wideScreenStyle.id = 'wideScreen-mode' // for isModeOn()
+    wideScreenStyle.innerHTML = '.text-base { max-width: 96% !important }'
+
+    // Create full window style
+    var fullWindowStyle = document.createElement('style')
+    fullWindowStyle.id = 'fullWindow-mode' // for isModeOn()
+    fullWindowStyle.innerHTML = sidebarClasses + '{ display: none }' // hide sidebar
+                              + sidepadClasses + '{ padding-left: 0px }' // remove side padding
 
     // Define SVG viewbox/paths
     var svgViewBox = '8 8 ' // move to XY coords to crop whitespace
                    + '20 20' // shrink to 20x20 to match Send button size
-    var theaterONpaths = `
-        <path fill="${ inputButtonColor }" fill-rule="evenodd"
+    var wideScreenONpaths = `
+        <path fill="${ sendButtonColor }" fill-rule="evenodd"
             d="m 26,13 0,10 -16,0 0,-10 z m -14,2 12,0 0,6 -12,0 0,-6 z"></path>`
-    var theaterOFFpaths = `
-        <path fill="${ inputButtonColor }" fill-rule="evenodd"
+    var wideScreenOFFpaths = `
+        <path fill="${ sendButtonColor }" fill-rule="evenodd"
             d="m 28,11 0,14 -20,0 0,-14 z m -18,2 16,0 0,10 -16,0 0,-10 z"></path>`
     var fullWindowONpaths = `
-        <path fill="${ inputButtonColor }" d="m 14,14 -4,0 0,2 6,0 0,-6 -2,0 0,4 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="m 22,14 0,-4 -2,0 0,6 6,0 0,-2 -4,0 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="m 20,26 2,0 0,-4 4,0 0,-2 -6,0 0,6 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="m 10,22 4,0 0,4 2,0 0,-6 -6,0 0,2 0,0 z"></path>`
+        <path fill="${ sendButtonColor }" d="m 14,14 -4,0 0,2 6,0 0,-6 -2,0 0,4 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="m 22,14 0,-4 -2,0 0,6 6,0 0,-2 -4,0 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="m 20,26 2,0 0,-4 4,0 0,-2 -6,0 0,6 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="m 10,22 4,0 0,4 2,0 0,-6 -6,0 0,2 0,0 z"></path>`
     var fullWindowOFFpaths = `
-        <path fill="${ inputButtonColor }" d="m 10,16 2,0 0,-4 4,0 0,-2 L 10,10 l 0,6 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="m 20,10 0,2 4,0 0,4 2,0 L 26,10 l -6,0 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="m 24,24 -4,0 0,2 L 26,26 l 0,-6 -2,0 0,4 0,0 z"></path>
-        <path fill="${ inputButtonColor }" d="M 12,20 10,20 10,26 l 6,0 0,-2 -4,0 0,-4 0,0 z"></path>`
+        <path fill="${ sendButtonColor }" d="m 10,16 2,0 0,-4 4,0 0,-2 L 10,10 l 0,6 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="m 20,10 0,2 4,0 0,4 2,0 L 26,10 l -6,0 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="m 24,24 -4,0 0,2 L 26,26 l 0,-6 -2,0 0,4 0,0 z"></path>
+        <path fill="${ sendButtonColor }" d="M 12,20 10,20 10,26 l 6,0 0,-2 -4,0 0,-4 0,0 z"></path>`
 
-    // Create Theater button & add classes/SVG/position/listener
-    var theaterButton = document.createElement('button') // create button
-    theaterButton.setAttribute('class', sendButtonClasses) // assign borrowed classes
-    updateSVG('theater') // insert SVG
-    theaterButton.style.cssText = 'right: 3.83rem' // position left of Send button
-    theaterButton.addEventListener( // add click listener
-        'click', () => { toggleMode('theater') }) // to toggle
+    // Create wide screen button & add classes/SVG/position/listeners
+    var wideScreenButton = document.createElement('button') // create button
+    wideScreenButton.id = 'wideScreen-button' // for toggleTooltip()
+    wideScreenButton.setAttribute('class', sendButtonClasses) // assign borrowed classes
+    updateSVG('wideScreen') // insert SVG
+    wideScreenButton.style.cssText = 'right: 3.83rem' // position left of Send button
+    wideScreenButton.addEventListener( 'click', () => { toggleMode('wideScreen') })
+    wideScreenButton.addEventListener( 'mouseover', (event) => { toggleTooltip() })
+    wideScreenButton.addEventListener( 'mouseout', (event) => { toggleTooltip() })
 
-    // Create Full-Window button & add classes/SVG/position/listener
+    // Create full window button & add classes/SVG/position/listeners
     var fullWindowButton = document.createElement('button') // create button
+    fullWindowButton.id = 'fullWindow-button' // for toggleTooltip()
     fullWindowButton.setAttribute('class', sendButtonClasses) // assign borrowed classes
     updateSVG('fullWindow') // insert SVG
-    fullWindowButton.style.cssText = 'right: 2.17rem' // position left of Theater button
-    fullWindowButton.addEventListener('click', () => { // add click listener
-        if (!isModeON('fullWindow')) toggleMode('theater', 'ON') // to be fuller screen
-        toggleMode('fullWindow') // or just toggle
-    })
+    fullWindowButton.style.cssText = 'right: 2.17rem' // position left of wide screen button
+    fullWindowButton.addEventListener('click', () => { // on clicks
+        if (!document.getElementById('fullWindow-mode')) { // if not full-window
+            toggleMode('wideScreen', 'ON') } // then make fuller screen
+        toggleMode('fullWindow') }) // but toggle full-window in any case
+    fullWindowButton.addEventListener( 'mouseover', (event) => { toggleTooltip() })
+    fullWindowButton.addEventListener( 'mouseout', (event) => { toggleTooltip() })
 
     insertToggles() // on page load
 
-    // Monitor node changes to retain buttons + auto-toggle once
-    var virginToggled = false
+    // Monitor node changes to maintain button visibility + auto-toggle once
+    var iniStateChecked = false, wideScreenState, fullWindowState
     var navObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.type === 'childList' && mutation.addedNodes.length) {
                 insertToggles()
-                if (!virginToggled) { // load keys to restore previous session's state
-                    if (localStorage.getItem('chatGPT_theater') == 'on') toggleMode('theater', 'ON')
+                if (!iniStateChecked) { // load keys to restore previous session's state
+                    if (localStorage.getItem('chatGPT_wideScreen') == 'on') toggleMode('wideScreen', 'ON')
                     if (localStorage.getItem('chatGPT_fullWindow') == 'on') toggleMode('fullWindow', 'ON')
-                    virginToggled = true
+                    iniStateChecked = true
                 }
     }})})
     navObserver.observe(document.documentElement, {childList: true, subtree: true})
@@ -91,18 +123,19 @@
     }
 
     function updateSVG(mode) {
-        var button = mode == 'theater' ? theaterButton : fullWindowButton
-        var ONpaths = mode == 'theater' ? theaterONpaths : fullWindowONpaths
-        var OFFpaths = mode == 'theater' ? theaterOFFpaths : fullWindowOFFpaths
+        var button = mode == 'wideScreen' ? wideScreenButton : fullWindowButton
+        var ONpaths = mode == 'wideScreen' ? wideScreenONpaths : fullWindowONpaths
+        var OFFpaths = mode == 'wideScreen' ? wideScreenOFFpaths : fullWindowOFFpaths
 
         // Initialize rem margin offset vs. OpenAI's .mr-1 for hover overlay centeredness
-        var lMargin = mode == 'theater' ? .11 : .12
+        var lMargin = mode == 'wideScreen' ? .11 : .12
         var rMargin = (.25 - lMargin)
 
         // Update SVG
         button.innerHTML = '<svg '
             + `class="${ sendSVGclasses }" ` // assign borrowed classes
-            + `style="margin: 0 ${ rMargin }rem 0 ${ lMargin }rem" `
+            + `style="margin: 0 ${ rMargin }rem 0 ${ lMargin }rem ; ` // center overlay
+            + `pointer-events: none" ` // prevent triggering tooltips twice
             + `viewBox="${ svgViewBox }"> ` // set viewbox pre-tweaked to match Send
             + (localStorage.getItem( // dynamically insert paths based on cached key
                 'chatGPT_' + mode) == 'on' ? ONpaths : OFFpaths + '</svg>')
@@ -111,37 +144,38 @@
     function insertToggles() {
         var chatbar = document.querySelector("form button[class*='bottom']").parentNode
         if (!chatbar.contains(fullWindowButton)) { // if toggles missing from chatbar
-            chatbar.append(fullWindowButton, theaterButton) // add them
+            chatbar.append(fullWindowButton, wideScreenButton, tooltipDiv) // add them + tooltip
     }}
 
-    function isModeON(mode) { // hard check to avoid faulty storage
-        var modeStyle = document.getElementById(mode + '-mode')
-        return modeStyle ? true : false
-    }
-
+    var wideScreenState = 'off', fullWindowState = 'off'
     function toggleMode(mode, state = '') {
-
-        var modeCSS = ( mode == // build CSS based on mode
-            'theater' ? '.text-base { max-width: 96% !important }' : // if Theater, expand txt to 96% of txt window
-            'fullWindow' ? sidebarClasses + '{ display: none }' // if Full-Window, hide sidebar...
-                + sidepadClasses + '{ padding-left: 0px }' : '' ) // + remove sidebar padding
-
         var modeStyle = document.getElementById(mode + '-mode') // look for existing style node
         if (state.toUpperCase() == 'ON' || !modeStyle ) { // if missing or ON-state passed
-
-            // Activate mode
-            modeStyle = document.createElement('style')
-            modeStyle.id = mode + '-mode' // assign ID for isModeOn()
-            modeStyle.appendChild(document.createTextNode(modeCSS))
-            document.head.appendChild(modeStyle)
-            localStorage.setItem('chatGPT_' + mode, 'on')
-
+            modeStyle = mode == 'wideScreen' ? wideScreenStyle : fullWindowStyle
+            document.head.appendChild(modeStyle) ; state = 'on' // activate mode
         } else { // de-activate mode
-            modeStyle.parentNode.removeChild(modeStyle)
-            localStorage.setItem('chatGPT_' + mode, 'off')
+            modeStyle.parentNode.removeChild(modeStyle) ; state = 'off'
         }
+        mode == 'wideScreen' ? wideScreenState = state : fullWindowState = state
+        localStorage.setItem('chatGPT_' + mode, state) // cache for new sessions
+        updateSVG(mode) ; updateTooltip(mode, state) // update icon/tooltip
+    }
 
-        updateSVG(mode) // to reflect change in button
+    function toggleTooltip() {
+        if (event.type === 'mouseover') {
+            var [buttonType, modeState] = ( event.target.id.includes('wide') ?
+                ['wideScreen', wideScreenState] : ['fullWindow', fullWindowState] )
+            updateTooltip(buttonType, modeState)
+            tooltipDiv.style.opacity = '0.8' // make visible
+        } else { tooltipDiv.style.opacity = '0' }
+    }
+
+    function updateTooltip(buttonType, modeState) { // text & position
+        tooltipDiv.innerHTML = tooltips[buttonType + modeState.toUpperCase()]
+        var ctrAddend = 17, overlayWidth = 30
+        var iniRoffset = overlayWidth * ( buttonType.includes('Window') ? 1 : 2 ) + ctrAddend
+        tooltipDiv.style.right = `${ // horizontal position
+                iniRoffset - tooltipDiv.getBoundingClientRect().width / 2 }px`
     }
 
 })()
