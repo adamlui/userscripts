dbb9d5173f05eed5b164abbf08d27080e2eca587
Adam Lui <adamlui@protonmail.com>
2023-03-11 08:02:18 -0800

    Switched from `localStorage` to `GM_` methods for security...
    + Created `saveSetting()` +  `loadSetting` for easier modification of storage API in ports
    + Renamed `styleNode` to `switchStyle` for clarity
    + Replaced click listener w/ `addEventListener` & updated `toggleAutoclear()` to work in VM
    + Added `display: flex` to `toggleLabel` style to overcome sporadic `display: box` (#19)
    + Added `toggleHidden` storage key
    + Added Chinese comments by @XiaoYingYo
    + Added `@author` + `@namespace`'s

--- chatgpt/autoclear-chatgpt-history/autoclear-chatgpt-history.user.js
+++ chatgpt/autoclear-chatgpt-history/autoclear-chatgpt-history.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name             Autoclear ChatGPT History
-// @version          2023.03.04.11
-// @author           Adam Lui & Tripp1e
-// @namespace        https://github.com/adamlui
+// @version          2023.03.11
+// @author           Adam Lui, Tripp1e & Xiao-Ying Yo
 // @description      Auto-clears chat history when visiting chat.openai.com
+// @namespace        https://github.com/adamlui
+// @namespace        https://github.com/Tripp1e
+// @namespace        https://github.com/XiaoYingYo
 // @homepageURL      https://github.com/adamlui/userscripts
 // @supportURL       https://github.com/adamlui/userscripts/issues
 // @license          MIT
@@ -17,16 +19,18 @@
 // @compatible       vivaldi
 // @match            https://chat.openai.com/*
 // @run-at           document-end
-// @grant            none
+// @grant            GM_setValue
+// @grant            GM_getValue
 // @updateURL        https://greasyfork.org/scripts/460805/code/autoclear-chatgpt-history.meta.js
 // @downloadURL      https://greasyfork.org/scripts/460805/code/autoclear-chatgpt-history.user.js
 // ==/UserScript==
 
-// Stylize toggle switch
-var styleNode = document.createElement('style')
-styleNode.innerHTML = `
+// Load settings
+var config = {} ; loadSetting('autoclear', 'toggleHidden')
 
-        /* Stylize switch */
+// Stylize toggle switch
+var switchStyle = document.createElement('style')
+switchStyle.innerHTML = `/* Stylize switch */
         .switch { position:absolute ; right:22px ; width:34px ; height:18px }
         .switch input { opacity:0 ; width:0 ; height:0 } /* hide checkbox */
         .slider { position:absolute ; cursor:pointer ; top:0 ; left:0 ; right:0 ; bottom:0 ; background-color:#ccc ; -webkit-transition:.4s ; transition:.4s ; border-radius:28px }
@@ -40,18 +44,18 @@ styleNode.innerHTML = `
             -ms-transform: translateX(14px) translateY(1px) ;
             transform: translateX(14px) }`
 
-document.head.appendChild(styleNode)
+document.head.appendChild(switchStyle)
 
-// Create toggle label & add classes/HTML
+// Create toggle label & add listener/classes/HTML
 var toggleLabel = document.createElement("div") // create label div
-toggleLabel.setAttribute("onclick", "window.toggleAutoclear()") // link to toggleAutoclear()
+toggleLabel.addEventListener( 'click', (event) => { toggleAutoclear(event) })
 for (var link of document.querySelectorAll('a')) { // inspect sidebar links for classes
     if (link.innerHTML.includes('New chat')) { // focus on 'New chat'
         toggleLabel.setAttribute("class", link.classList) // borrow its classes
         break // stop looping since class assignment is done
 }} ; updateToggleHTML()
 
-// Insert full toggle on page load + during navigation
+// Insert full toggle on page load + during navigation // 在导航期间插入页面加载 + 的完整切换
 insertToggle()
 var navObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
@@ -60,40 +64,42 @@ var navObserver = new MutationObserver(function(mutations) {
 }})})
 navObserver.observe(document.documentElement, {childList: true, subtree: true})
 
-// Auto-clear chats if activated
+// Auto-clear chats if activated // 自动清除聊天是否激活
 var labels = ['Clear conversations', 'Confirm clear conversations']
 var clearObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (mutation.addedNodes[0]?.innerHTML.includes(labels[0])) {
             clearAllMsgs() ; clearObserver.disconnect() }})
-        // Also disconnect after 5sec to avoid clearing new convos
+        // Also disconnect after 5sec to avoid clearing new chats // 还要在5秒后断开连接,以避免清除新的频道
         setTimeout(function() { clearObserver.disconnect() }, 5000)
 })
-if (localStorage.getItem('chatGPT_autoclear') == 'true') {
+if (loadSetting('autoclear') === true) {
     clearObserver.observe(document, {childList: true, subtree: true}) }
 
 
-// Functions
+// Functions // 功能
 
 function updateToggleHTML() {
     toggleLabel.innerHTML = `
         <img width="18px" src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/autoclear-chatgpt-history/navicon.png">
-        Auto-clear ${localStorage.getItem('chatGPT_autoclear') == 'true' ? "enabled" : "disabled"}
+        Auto-clear ${ config['autoclear'] ? 'enabled' : 'disabled' }
         <label class="switch" ><input id="autoclearToggle" type="checkbox"
-            ${localStorage.getItem('chatGPT_autoclear') == 'true' ? "checked='true'" : ""} >
+            ${ config['autoclear'] ? "checked='true'" : ""} >
             <span class="slider"></span></label>`
+    toggleLabel.style.display = config['toggleHidden'] ? 'none' : 'flex'
 }
 
 function insertToggle() {
     for (var nav of document.querySelectorAll('nav')) {
-        if (!nav.contains(toggleLabel)) { // check if label exists first
-            nav.insertBefore(toggleLabel, nav.childNodes[0]) // insert before 'New chat'
+        if (!nav.contains(toggleLabel)) { // check if label exists first // 检查标签是否首先存在
+            nav.insertBefore(toggleLabel, nav.childNodes[0]) // insert before 'New chat'// 在"新聊天"之前插入
 }}}
 
-window.toggleAutoclear = function() {
-    document.querySelector('#autoclearToggle').click()
-    localStorage.setItem( // save setting
-        'chatGPT_autoclear', document.querySelector('#autoclearToggle').checked)
+toggleAutoclear = function() {
+    if (event.target == toggleLabel) { // to avoid double-toggle
+        document.querySelector('#autoclearToggle').click() }
+    saveSetting( // save setting
+        'autoclear', document.querySelector('#autoclearToggle').checked)
     setTimeout(updateToggleHTML, 200) // sync label change w/ switch movement
 }
 
@@ -105,3 +111,13 @@ function clearAllMsgs() {
             link.click() ; labelCnt++
             setTimeout(clearAllMsgs, 500) ; return // repeat to confirm
 }}}
+
+function saveSetting(key, value) {
+    GM_setValue('chatGPT_' + key, value) // save to browser
+    config[key] = value // and memory
+}
+
+function loadSetting(...keys) {
+    keys.forEach(function(key) {
+        config[key] = GM_getValue('chatGPT_' + key)
+})}
