95c95caf770cbae031daceae56504e8135fbe5bb
adamlui@protonmail.com <adamlui@protonmail.com>
2023-03-25 23:43:10 -0700

    Removed submodule dependency

---
+++ chatgpt/duckduckgpt/README.md
@@ -0,0 +1,81 @@
+<h1>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/adamlui/duckduckgpt/main/media/images/ddgpt-logo-dark-mode-371x65.png">
+    <img alt="DuckDuckGPT" src="https://raw.githubusercontent.com/adamlui/duckduckgpt/main/media/images/ddgpt-logo-light-mode-371x65.png">
+  </picture>
+<a href="https://www.producthunt.com/posts/duckduckgpt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-duckduckgpt" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=379261&theme=light" alt="DuckDuckGPT - Bring&#0032;the&#0032;magic&#0032;of&#0032;ChatGPT&#0032;to&#0032;DuckDuckGo | Product Hunt" style="width: 156px; height: 34px; margin:0 0 11px 5px;" width="156" height="34" /></a>
+  <a href="https://twitter.com/intent/tweet?text=Want%20%23ChatGPT%20answers%20right%20alongside%20DuckDuckGo%20results%3F%20This%20%23userscript%20does%20just%20that%21&url=https://duckduckgpt.com&hashtags=greasemonkey,javascript,ai"><img alt="Tweet" style="margin:0 0 14px 5px;" src="https://img.shields.io/twitter/url/http/shields.io.svg?style=social"></a>
+</h1>
+
+![usercount](https://img.shields.io/greasyfork/dt/459849?label=Users)
+[![license](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+[![support](https://img.shields.io/badge/Support-Chrome/Firefox/Edge/Opera/Brave/Vivaldi/LibreWolf/QQ-989898.svg)](#compatibility)
+
+<h3><b>DuckDuckGPT</b> brings the magic of ChatGPT to search results.</h3>
+
+<a href="https://greasyfork.org/en/scripts/459849-duckduckgpt"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) /
+[Review](https://duckduckgpt.com/userscript/review) /
+[Contact author](https://elonsucks.org/@adam)
+
+#
+
+<img src="https://i.imgur.com/emsmP6p.png">
+
+## Installation
+
+1. Install Tampermonkey ([Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo), [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd), [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)) or Violentmonkey ([Chrome](https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag), [Firefox](https://addons.mozilla.org/firefox/addon/violentmonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao))
+
+2. Install [DuckDuckGPT](https://greasyfork.org/en/scripts/459849-duckduckgpt) (will load in userscript manager installed above)
+
+3. Visit [duckduckgo.com](https://duckduckgo.com), type your search, engage bot!
+
+![ddgpt-demo](https://user-images.githubusercontent.com/10906554/220238659-9b59de4f-31d0-468a-a41d-528ac924732f.gif)
+
+## Compatibility
+
+DuckDuckGPT is compatible with Chrome, Firefox, Edge, Brave, Opera, Vivaldi, LibreWolf & QQ using Tampermonkey (recommended due to streaming suppport resulting in faster responses) or Violentmonkey. ([Pull requests](https://github.com/adamlui/userscripts/pulls) always welcome!)
+
+<a href="https://greasyfork.org/en/scripts/459849-duckduckgpt"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) /
+[Leave a review](https://duckduckgpt.com/userscript/review) /
+[More ChatGPT userscripts](https://github.com/adamlui/userscripts/tree/master/chatgpt)
+
+## Social links
+
+<a href="https://technodon.org/@duckduckgpt" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/109876284007792895?domain=https%3A%2F%2Ftechnodon.org&style=social" alt="Follow @duckduckgpt on Mastodon" title="Mastodon"></a>
+[![Follow @adamlui on GitHub](https://img.shields.io/github/followers/adamlui?label=Follow%20%40adamlui&style=social "GitHub")](https://github.com/adamlui)
+[![Follow @AdamLuiTube on YouTube](https://img.shields.io/youtube/channel/subscribers/UCgBMqK7SRL5R__3qM-YAcSg?label=Follow%20%40AdamLuiTube&style=social)](https://www.youtube.com/AdamLuiTube?sub_confirmation=1)
+<a href="https://elonsucks.org/@adam" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/109387703022229926?domain=https%3A%2F%2Felonsucks.org&style=social" alt="Follow @adam on Mastodon" title="Mastodon">
+[![Follow @adamllui on Twitter](https://img.shields.io/twitter/follow/adamllui?style=social)](https://twitter.com/adamllui)
+<br><br>
+
+## Related scripts
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Auto Refresh ↻](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-auto-refresh)
+
+Keeps your ChatGPT sessions active to avoid constant Cloudflare checks.
+<br>[Install](https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh) / 
+[Readme](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-auto-refresh/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <img src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/bravegpt-icon48.png" width=18> [BraveGPT](https://github.com/adamlui/userscripts/blob/master/chatgpt/bravegpt) <a href="https://www.producthunt.com/posts/bravegpt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-bravegpt" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=385630&theme=light" alt="BraveGPT - Bring&#0032;the&#0032;magic&#0032;of&#0032;ChatGPT&#0032;to&#0032;Brave&#0032;Search&#0033; | Product Hunt" style="width: 112px; height: 24px; margin:0 0 -4px 5px;" width="112" height="24" /></a>
+
+Display ChatGPT answers in Brave Search sidebar. // 在 Brave Search 侧边栏中显示 ChatGPT 答案.
+<br>[Install](https://greasyfork.org/en/scripts/462440-bravegpt) / 
+[Readme](https://github.com/adamlui/userscripts/blob/master/chatgpt/bravegpt/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Widescreen Mode](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-widescreen-mode)
+
+Adds Widescreen + Full-Window modes to ChatGPT for enhanced viewing. // 向 ChatGPT 添加宽屏 + 全窗口模式以减少滚动。
+<br>[Install](https://greasyfork.org/en/scripts/461473-chatgpt-widescreen-mode) / 
+[Readme](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-widescreen-mode/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+#
+
+<a href="https://github.com/adamlui/userscripts">**More userscripts**</a> / 
+<a href="#--------------">Back to top ↑</a>
---
+++ chatgpt/duckduckgpt/duckduckgpt.user.js
@@ -0,0 +1,227 @@
+// ==UserScript==
+// @name             DuckDuckGPT 🤖
+// @version          2023.03.22.1
+// @author           Adam Lui
+// @namespace        https://github.com/adamlui
+// @description      Adds ChatGPT answers to DuckDuckGo sidebar
+// @homepageURL      https://www.duckduckgpt.com
+// @supportURL       https://github.com/adamlui/userscripts/issues
+// @license          MIT
+// @icon             https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/duckduckgpt/media/images/ddgpt-icon48.png
+// @icon64           https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/duckduckgpt/media/images/ddgpt-icon64.png
+// @compatible       chrome
+// @compatible       firefox
+// @compatible       edge
+// @compatible       opera
+// @compatible       brave
+// @compatible       vivaldi
+// @compatible       librewolf
+// @compatible       qq
+// @match            https://duckduckgo.com/*
+// @include          https://auth0.openai.com
+// @connect          chat.openai.com
+// @grant            GM.deleteValue
+// @grant            GM.getValue
+// @grant            GM.setValue
+// @grant            GM.xmlHttpRequest
+// @grant            GM_cookie
+// @updateURL        https://www.duckduckgpt.com/userscript/code/duckduckgpt.meta.js
+// @downloadURL      https://www.duckduckgpt.com/userscript/code/duckduckgpt.user.js
+// ==/UserScript==
+
+var GM_setValue = (() => GM.setValue)()
+var GM_deleteValue = (() => GM.deleteValue)()
+var GM_xmlhttpRequest = (() => GM.xmlHttpRequest)()
+var GM_getValue = (() => GM.getValue)()
+
+function getUserscriptManager() {
+    try { return GM_info.scriptHandler } catch (error) { return "other" }}
+
+function deleteOpenAIcookies() {
+    if (getUserscriptManager() !== "Tampermonkey") return
+    var openAIauthURL = 'https://auth0.openai.com'
+    GM_cookie.list({ url: openAIauthURL }, function(cookies, error) {
+        if (!error) { for (var i = 0; i < cookies.length; i++) {
+            GM_cookie.delete({ url: openAIauthURL, name: cookies[i].name })
+}}})}
+
+function uuidv4() {
+    var s = [], hexDigits = "0123456789abcdef"
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 16), 1) }
+    s[14] = "4"
+    s[19] = hexDigits.substr(s[19] & 3 | 8, 1)
+    s[8] = s[13] = s[18] = s[23] = "-"
+    var uuid = s.join("")
+    return uuid
+}
+
+var container = document.createElement("div")
+function getContainer() { return container }
+
+var alerts = {
+    waitingResponse: "Waiting for ChatGPT response...",
+    login: "Please login @ ",
+    tooManyRequests: "ChatGPT is flooded with too many requests. Check back later!",
+    checkCloudflare: "Please pass Cloudflare security check @ ",
+    unknowError: "An unknown error has occurred",
+    networkException: "Network exception, please refresh the page",
+}
+function chatGPTalert(msg) {
+    if (msg.includes('login')) deleteOpenAIcookies()
+    container.innerHTML = (
+        /waiting|loading/i.test(msg) ? // if alert involves loading, add class
+            '<p class="loading">' : '<p>') + alerts[msg]
+        + (alerts[msg].includes('@') ? // if msg needs a link, add it
+            '<a href="https://chat.openai.com" target="_blank">chat.openai.com</a></p>' : '</p>')
+}
+
+function containerShow(answer) {
+    var container2 = getContainer()
+    container2.innerHTML = '<p><span class="prefix">ChatGPT</span><pre></pre></p>'
+    container2.querySelector("pre").textContent = answer
+}
+
+function containerAlert(htmlStr) {
+    var container2 = getContainer()
+    container2.innerHTML = htmlStr
+}
+
+function isBlockedbyCloudflare(resp) {
+    try {
+        var html = new DOMParser().parseFromString(resp, "text/html")
+        var title = html.querySelector("title")
+        return title.innerText === "Just a moment..."
+    } catch (error) { return false }
+}
+
+var timeoutPromise = new Promise((resolve, reject) => {
+  setTimeout(() => { reject(new Error('Timeout occurred')) }, 3000) })
+
+async function getAnswer(question, callback) {
+    try {
+        var accessToken = await Promise.race([getAccessToken(), timeoutPromise])
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://chat.openai.com/backend-api/conversation",
+            headers: {
+                "Content-Type": "   application/json",
+                Authorization: `Bearer ${accessToken}`
+            },
+            responseType: responseType(),
+            data: JSON.stringify({
+                action: "next",
+                messages: [
+                    {
+                        id: uuidv4(),
+                        role: "user",
+                        content: {
+                            content_type: "text",
+                            parts: [question]
+                        }
+                    }
+                ],
+                model: "text-davinci-002-render",
+                parent_message_id: uuidv4()
+            }),
+            onloadstart: onLoadStart(),
+            onload: onLoad()
+        })
+    } catch (error) {
+        if (error === "UNAUTHORIZED") {
+            GM_deleteValue("accessToken")
+            chatGPTalert('login')
+        }
+        console.error("getAnswer error: ", error)
+    }
+    function responseType() {
+      if (getUserscriptManager() === "Tampermonkey") {
+        return "stream" } else { return "text" }
+    }
+    function onLoad() {
+        return function(event) {
+            if (event.status === 401) { GM_deleteValue("accessToken") ; chatGPTalert('login') }
+            if (event.status === 403) { chatGPTalert('checkCloudflare') }
+            if (event.status === 429) { chatGPTalert('tooManyRequests') }
+            if (getUserscriptManager() !== "Tampermonkey") {
+                if (event.response) {
+                    const answer = JSON.parse(event.response
+                        .split("\n\n").slice(-3, -2)[0].slice(6)).message.content.parts[0]
+                    containerShow(answer)
+        }}}
+    }
+    function onLoadStart() {
+        if (getUserscriptManager() === "Tampermonkey") {
+            return function(stream) {
+                var reader = stream.response.getReader()
+                reader.read().then(function processText({ done, value }) {
+                    if (done) { return }
+                    let responseItem = String.fromCharCode(...Array.from(value))
+                    var items = responseItem.split("\n\n")
+                    if (items.length > 2) {
+                        var lastItem = items.slice(-3, -2)[0]
+                        if (lastItem.startsWith("data: [DONE]")) {
+                            responseItem = items.slice(-4, -3)[0]
+                        } else { responseItem = lastItem }
+                    }
+                    if (responseItem.startsWith("data: {")) {
+                        var answer = JSON.parse(responseItem.slice(6)).message.content.parts[0]
+                        containerShow(answer)
+                    } else if (responseItem.startsWith("data: [DONE]")) { return }
+                    return reader.read().then(processText)
+    })}}}
+}
+
+function getAccessToken() {
+    return new Promise(async (resolve, rejcet) => {
+        var accessToken = await GM_getValue("accessToken")
+        if (!accessToken) {
+            GM_xmlhttpRequest({
+                url: "https://chat.openai.com/api/auth/session",
+                onload: function(response) {
+                    if (isBlockedbyCloudflare(response.responseText)) {
+                        chatGPTalert('login') ; return }
+                    var accessToken2 = JSON.parse(response.responseText).accessToken;
+                    if (!accessToken2) { rejcet("UNAUTHORIZED") }
+                    GM_setValue("accessToken", accessToken2)
+                    resolve(accessToken2)
+                },
+                onerror: function(error) { rejcet(error) },
+                ontimeout: () => { console.error("getAccessToken timeout!") }
+            })
+        } else { resolve(accessToken) }
+})}
+
+// Stylize ChatGPT container to match DDG's
+var css = `
+    .chatgpt-container { border-radius: 8px ; border: 1px solid #dadce0 ; padding: 15px ; flex-basis: 0 ;
+        flex-grow: 1 ; word-wrap: break-word ; white-space: pre-wrap ; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.06) }
+    .chatgpt-container p { margin: 0 }
+    .chatgpt-container .prefix { font-weight: 700 }
+    .chatgpt-container .loading { color: #b6b8ba ; animation: pulse 2s cubic-bezier(.4,0,.6,1) infinite }
+    .chatgpt-container.sidebar-free { margin-left: 60px ; height: fit-content }
+    .chatgpt-container pre { white-space: pre-wrap ; min-width: 0 ; margin-bottom: 0 ; line-height: 20px }
+    @keyframes pulse { 0%, to { opacity: 1 } 50% { opacity: .5 }}
+    .chatgpt-feedback { margin: 2px 0 25px 0 }`
+var styleNode = document.createElement('style') ; styleNode.innerText = css
+document.head.appendChild(styleNode)
+
+// Run injection function
+async function main() {
+
+        // Initialize container
+        var container2 = getContainer()
+        container2.className = "chatgpt-container"
+        chatGPTalert('waitingResponse')
+
+        // Initialize feedback link
+        var container3 = document.createElement("div")
+        container3.className = "feedback-prompt chatgpt-feedback"
+        container3.innerHTML = `<a href="https://github.com/adamlui/userscripts/discussions/new/choose" class="feedback-prompt__link" target="_blank">Share Feedback</a>`
+
+        // Inject container + feedback link
+        var siderbarContainer = document.getElementsByClassName("results--sidebar")[0]
+        siderbarContainer.prepend(container2, container3)
+        getAnswer(new URL(window.location.href).searchParams.get("q"))
+
+} ; main()
