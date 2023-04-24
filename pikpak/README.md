d0b18acc3d645a6f8d425348c019c2b93d41ae02
XiaoYingYo <759852125@qq.com>
2023-04-25 03:15:37 +0800

    New feature and compatible with Pikpak routing

--- pikpak/README.md
+++ pikpak/README.md
@@ -18,10 +18,16 @@ Ps:Running these scripts requires a user script manager
 3. In the `Recently Added` tab, when clicking `Delete` for the right-click menu, click Delete immediately without confirmation
 4. In the `Recently Added` tab, when clicking `Download` for the right-click menu, click the browser to download immediately without confirmation
 5. In the `Recycle Bin` tab, when clicking `Permanent Delete` for the right-click menu, click Permanent Delete immediately without confirmation
+6. Remove the download progress pop-up window that pops up for the first cloud download, and it will no longer be displayed automatically next time
+7. Remove the cookie agreement that pops up when you log in for the first time, and automatically accept all cookie permissions
+8. When using the mailbox to log in on the login interface, add automatic identification of the mailbox and password, and automatically fill in and log in automatically
 - 中文解释
 1. 在`全部文件`选项卡时,为右键菜单点击`删除`时,无需确认立即点击删除
 2. 在`全部文件`选项卡时,为右键菜单点击`下载`时,无需确认立即点击浏览器下载
 3. 在`最近添加`选项卡时,为右键菜单点击`删除`时,无需确认立即点击删除
 4. 在`最近添加`选项卡时,为右键菜单点击`下载`时,无需确认立即点击浏览器下载
 5. 在`回收站`选项卡时,为右键菜单点击`永久删除`时,无需确认立即点击永久删除
+6. 移除第一次云下载弹出的下载进度弹窗,自动不再下次不再显示
+7. 移除第一次登录时弹窗的Cookie协议,自动接受全部Cookie许可
+8. 在登录界面使用邮箱登录时,新增自动识别邮箱和密码,并自动填充并自动登录
 <br>[View scripts](pikpak-enhance) / [Discuss](https://github.com/adamlui/userscripts/discussions)
--- pikpak/pikpak-enhance/pikpak-enhance.js
+++ pikpak/pikpak-enhance/pikpak-enhance.js
@@ -29,22 +29,32 @@ var $ = window["$$$"];
 
 var GlobalVariable = {};
 
-async function DealWithoverlay(i) {
+async function DealWithoverlay(i, callback) {
     return new Promise(async (resolve) => {
         let overlay = $("div.el-overlay:not([style*='display'])").eq(0);
         if (overlay.length === 0) {
             resolve();
             return;
         }
-        switch (i) {
-            case 0:
-                overlay.find("button[class*='is-text']").eq(0).click();
-                break;
-            case 1:
-                overlay.find("button[class*='--primary']").eq(0).click();
-                break;
+        let btn = null;
+        if (i != null) {
+            switch (i) {
+                case 0:
+                    btn = overlay.find("button[class*='is-text']").eq(0);
+                    break;
+                case 1:
+                    btn = overlay.find("button[class*='--primary']").eq(0);
+                    break;
+            }
         }
-        overlay.hide();
+        if (typeof (callback) == "function") {
+            callback(overlay, btn);
+        } else {
+            if (btn != null) {
+                btn.click();
+            }
+            overlay.hide();
+        }
         resolve();
     });
 }
@@ -71,6 +81,49 @@ async function LiClick() {
 }
 
 GlobalVariable.MonitorMenuClickFun = global_module.debounce(MonitorMenuClick, 100);
+GlobalVariable.InterfacelanguageList = ["en", "zh-CN", "zh-TW", "ja", "ko", "de", "fr", "es", "pt", "ru", "it", "tr", "ar", "th", "vi", "id"];
+GlobalVariable.Navigatorlanguage = GlobalVariable.InterfacelanguageList.indexOf(navigator.language) != -1 ? navigator.language : "en";
+GlobalVariable.Interfacelanguage = {
+    "login": {
+        001: {
+            "en": "preferred use Sign in or Register with Email",
+            "zh-CN": "首选使用邮箱登录或注册",
+            "zh-TW": "首選使用郵箱登錄或註冊",
+            "ja": "メールでログインまたは登録することを好む",
+            "ko": "이메일로 로그인 또는 등록하는 것을 선호합니다",
+            "de": "Bevorzugte Anmeldung oder Registrierung per E-Mail",
+            "fr": "Préférez-vous vous connecter ou vous inscrire par e-mail",
+            "es": "Preferencia para iniciar sesión o registrarse con correo electrónico",
+            "pt": "Preferência para fazer login ou registrar com e-mail",
+            "ru": "Предпочтительный вход или регистрация по электронной почте",
+            "it": "Preferenza per accedere o registrarsi con e-mail",
+            "tr": "E-posta ile oturum açın veya kaydolun",
+            "ar": "تفضيل تسجيل الدخول أو التسجيل باستخدام البريد الإلكتروني",
+            "th": "การเข้าสู่ระบบหรือลงทะเบียนด้วยอีเมลที่ต้องการ",
+            "vi": "Ưu tiên sử dụng Đăng nhập hoặc Đăng ký bằng Email",
+            "id": "Pilihan untuk masuk atau mendaftar dengan email"
+        },
+        002: {
+            "en": "Enter your email address and password here to automatically try to log in",
+            "zh-CN": "在此输入您的电子邮件地址和密码，以自动尝试登录",
+            "zh-TW": "在此輸入您的電子郵件地址和密碼，以自動嘗試登錄",
+            "ja": "ここにメールアドレスとパスワードを入力して、自動的にログインを試みます",
+            "ko": "이메일 주소와 비밀번호를 입력하여 자동으로 로그인하십시오",
+            "de": "Geben Sie hier Ihre E-Mail-Adresse und Ihr Passwort ein, um sich automatisch anzumelden",
+            "fr": "Entrez votre adresse e-mail et votre mot de passe ici pour vous connecter automatiquement",
+            "es": "Ingrese su dirección de correo electrónico y contraseña aquí para iniciar sesión automáticamente",
+            "pt": "Insira seu endereço de e-mail e senha aqui para fazer login automaticamente",
+            "ru": "Введите здесь свой адрес электронной почты и пароль, чтобы автоматически войти в систему",
+            "it": "Inserisci qui il tuo indirizzo email e la tua password per accedere automaticamente",
+            "tr": "E-posta adresinizi ve şifrenizi buraya girin ve otomatik olarak oturum açmaya çalışın",
+            "ar": "أدخل عنوان بريدك الإلكتروني وكلمة المرور هنا للمحاولة تسجيل الدخول تلقائيًا",
+            "th": "ป้อนที่อยู่อีเมลและรหัสผ่านของคุณที่นี่เพื่อล็อกอินโดยอัตโนมัติ",
+            "vi": "Nhập địa chỉ email và mật khẩu của bạn ở đây để tự động đăng nhập",
+            "id": "Masukkan alamat email dan kata sandi Anda di sini untuk masuk secara otomatis"
+        }
+    }
+};
+
 
 async function MonitorMenuClick() {
     let Download = GlobalVariable.language["download"];
@@ -122,6 +175,7 @@ async function FindStr(obj) {
                         let func = new Function(scriptStr);
                         func();
                         GlobalVariable.language = unsafeWindow.__language__;
+                        unsafeWindow.GlobalVariable = GlobalVariable;
                         resolve();
                         return;
                     }
@@ -134,14 +188,183 @@ async function FindStr(obj) {
     });
 }
 
-async function main() {
-    let nav = await global_module.waitForElement("ul[class='nav']", null, null, 100, -1);
-    await FindStr(unsafeWindow.webpackChunkxlco_pikpak_web);
-    GlobalVariable.nav = nav;
-    let activeLi = await global_module.waitForElement("a[class^='active']", null, null, 100, -1, nav);
-    GlobalVariable.activeLi = activeLi;
-    nav.find("li").on("click", LiClick);
-    MonitorMenu();
+GlobalVariable.registerEdMenu = [];
+
+function OverloadMenu() {
+    for (let i = 0; i < GlobalVariable.registerEdMenu.length; i++) {
+        GM_unregisterMenuCommand(GlobalVariable.registerEdMenu[i]);
+    }
+    let L001 = GlobalVariable.Interfacelanguage["login"][001][GlobalVariable.Navigatorlanguage];
+    let V001 = GM_getValue("loginForEmail", false) ? "√" : "×";
+    GlobalVariable.registerEdMenu.push(GM_registerMenuCommand(L001 + " " + V001, function () {
+        GM_setValue("loginForEmail", !GM_getValue("loginForEmail", false));
+        OverloadMenu();
+    }));
 }
 
-main();
+function loginPaneleModified() {
+    let forgetPasswordElement = GlobalVariable.loginPanel.find("div[class*='forget-password']").eq(0);
+    if (forgetPasswordElement.length == 0) {
+        return;
+    }
+    let item = forgetPasswordElement.parent();
+    let form = item.parent().eq(0);
+    if (form.find("div#QuickInput").length != 0) {
+        return;
+    }
+    let formDivHeight = 0;
+    let QuickInputHeight = "";
+    let formHeight = form.height();
+    form.children().each(function () {
+        formDivHeight += $(this).outerHeight(true);
+    });
+    QuickInputHeight = formHeight - formDivHeight;
+    let div = $(`<div id="QuickInput"><textarea placeholder="` + GlobalVariable.Interfacelanguage["login"][002][GlobalVariable.Navigatorlanguage] + `" style="width: 100%; height:` + QuickInputHeight + `px;"></textarea></div>`);
+    form.append(div);
+    div.find("textarea").eq(0).on("input propertychange", global_module.debounce(function () {
+        let text = $(this).val();
+        let emailReg = /([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/g;
+        let email = text.match(emailReg);
+        if (email == null) {
+            return;
+        }
+        let password = "";
+        text = text.replace(emailReg, "");
+        text = text.replace(/\n/g, "");
+        email = email[0];
+        if (text.indexOf(":") != -1) {
+            let index = text.lastIndexOf(":");
+            password = text.substring(index + 1);
+        } else if (!/\s/.test(text)) {
+            password = text.replace(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\\\:\;\"\'\,\.\/\<\>\?]/g, "");
+        } else {
+            let index = text.lastIndexOf(" ");
+            password = text.substring(index + 1);
+        }
+        if (email != "" && password != "") {
+            let inputList = item.find("input[class][placeholder][type]");
+            global_module.AnalogInput.AnalogInput(inputList.eq(0)[0], email);
+            global_module.AnalogInput.AnalogInput(inputList.eq(1)[0], password);
+            let loginBtn = item.find("div[class*='login-button']").eq(0);
+            MonitorUrl(1);
+            setTimeout(function () {
+                loginBtn.click();
+            }, 200);
+        }
+    }, 200));
+}
+
+function StopMonitorUrl() {
+    if (GlobalVariable.MonitorUrlInterval != null) {
+        for (let i = 0; i < GlobalVariable.MonitorUrlInterval.length; i++) {
+            clearInterval(GlobalVariable.MonitorUrlInterval[i]);
+        }
+        GlobalVariable.MonitorUrlInterval = [];
+    }
+}
+
+function MonitorUrl(to) {
+    GlobalVariable.HistoricalUrlPath = unsafeWindow.location.pathname;
+    if (GlobalVariable.MonitorUrlInterval == null) {
+        GlobalVariable.MonitorUrlInterval = [];
+    }
+    StopMonitorUrl();
+    GlobalVariable.MonitorUrlInterval.push(setInterval(function () {
+        let path = unsafeWindow.location.pathname;
+        if (path == GlobalVariable.HistoricalUrlPath) {
+            return;
+        }
+        if (path.indexOf("/drive/login") != -1 && to == 0) {
+            login();
+        } else if (to == 1) {
+            main();
+        }
+        StopMonitorUrl();
+    }, 500));
+}
+
+async function login() {
+    let loginPanel = await global_module.waitForElement("div[class*='login-panel']", null, null, 100, -1);
+    loginPanel = loginPanel.eq(0);
+    GlobalVariable.loginPanel = loginPanel;
+    loginPanel.on("DOMSubtreeModified", global_module.debounce(loginPaneleModified, 200));
+    if (GM_getValue("loginForEmail", false)) {
+        let emailBtn = await global_module.waitForElement("div[class*='icon-email']", null, null, 100, -1);
+        emailBtn.eq(0).click();
+    }
+    Init(0);
+}
+
+async function MonitorLogout() {
+    let nav = await global_module.waitForElement("ul[class='user-nav']", null, null, 100, -1);
+    let li = nav.find("li").eq(-1);
+    li.on("click", function () {
+        DealWithoverlay(1, function (overlay, btn) {
+            if (btn != null) {
+                btn.on("click", function () {
+                    overlay.hide();
+                    global_module.Cookie.clear();
+                    unsafeWindow.location.href = "/drive/login";
+                });
+            }
+        });
+    });
+}
+
+async function main() {
+    MonitorUrl(0);
+    MonitorLogout();
+    let nav = await global_module.waitForElement("ul[class='nav']", null, null, 100, 60 * 1000);
+    if (nav == null) {
+        for (let i = 0; i < 5; i++) {
+            new Error("nav is null");
+        }
+        return;
+    }
+    await FindStr(unsafeWindow.webpackChunkxlco_pikpak_web);
+    GlobalVariable.nav = nav;
+    let activeLi = await global_module.waitForElement("a[class^='active']", null, null, 100, 60 * 1000, nav);
+    if (activeLi == null) {
+        for (let i = 0; i < 5; i++) {
+            new Error("activeLi is null");
+        }
+        return;
+    }
+    StopMonitorUrl();
+    GlobalVariable.activeLi = activeLi;
+    nav.find("li").on("click", LiClick);
+    $("svg[class='safety']").eq(0).remove();
+    MonitorMenu();
+    Init(1);
+}
+
+function Init(index) {
+    global_module.Cookie.set("allow_analysis", "true", 10 * 365 * 24 * 60 * 60 * 1000);
+    if (index == 0) {
+
+    } else if (index == 1) {
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            if (key.indexOf("credentials_") == -1) {
+                let json = JSON.parse(value);
+                let sub = json.sub;
+                global_module.Cookie.set("pp_disabled_speed_save_dialog_" + sub, "true", 10 * 365 * 24 * 60 * 60 * 1000);
+            }
+            break;
+        }
+    }
+}
+
+function Preload() {
+    OverloadMenu();
+    Init();
+    let href = unsafeWindow.location.href;
+    if (href.indexOf("/drive/login") != -1) {
+        login();
+    } else {
+        main();
+    }
+}
+
+Preload();
