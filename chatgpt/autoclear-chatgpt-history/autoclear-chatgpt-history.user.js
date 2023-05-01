572b32715358967ceb89a456931a2493e55952fe
小影哟 <759852125@qq.com>
2023-05-01 14:41:15 +0800

    Modify the version number

--- chatgpt/autoclear-chatgpt-history/autoclear-chatgpt-history.user.js
+++ chatgpt/autoclear-chatgpt-history/autoclear-chatgpt-history.user.js
@@ -11,7 +11,7 @@
 // @name:es             Borrar Automáticamente el Historial de ChatGPT
 // @name:fr             Effacement Automatique de L'Historique ChatGPT
 // @name:it             Cancella Automaticamente Cronologia ChatGPT
-// @version             2023.4.23
+// @version             2023.05.01
 // @description         Auto-clears chat history when visiting chat.openai.com
 // @author              Adam Lui (刘展鹏), Tripp1e & Xiao-Ying Yo (小影哟)
 // @namespace           https://github.com/adamlui
