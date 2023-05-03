1d8760630a540e5b7fb6b2e47e07b5775fc0be1e
adamlui@protonmail.com <adamlui@protonmail.com>
2023-05-02 20:34:40 -0700

    Implemented ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports  = {
+    'extends': ['eslint:recommended'],
+    'ignorePatterns': ['**/*.md'],
+    'rules': {
+        'indent': ['error', 4, { 'ignoredNodes': [ // enforce 4-space indentation, except for...
+            'TemplateLiteral > *', // template literal children
+            'ConditionalExpression', // ternarys
+            'IfStatement > BlockStatement', // multi-line if's
+            'BinaryExpression', // string concatenations, math, comparison, etc.,
+            'BlockStatement', 'SwitchCase' // closing braces
+        ]}],
+        'quotes': ['error', 'single'], // enforce single quotes for string literals
+        'space-before-function-paren': ['error', 'never'], // enforce no space before function parentheses
+        'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
+        'comma-dangle': ['error', 'never'] // enforce no trailing commas in arrays or objects
+    },
+    'parserOptions': {
+        'ecmaVersion': 2017,
+        'sourceType': 'script'
+    },
+    'env': { 'browser': true, 'node': true, 'es6': true }
+};
