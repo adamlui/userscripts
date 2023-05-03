dd5ab480dbd720c1c3ea6cd0a78aebd765373a8b
adamlui@protonmail.com <adamlui@protonmail.com>
2023-05-02 22:41:18 -0700

    Added ignoring of constant conditions

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports  = {
         'space-before-function-paren': ['error', 'never'], // enforce no space before function parentheses
         'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
         'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
-        'no-async-promise-executor': 'off' // allow promise executor functions to be async (to accomodate await lines)
+        'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
+        'no-constant-condition': 'off' // to accomodate while (true)
     },
     'globals': { 'chatgpt': true, 'global_module': true, '$': true },
     'parserOptions': {
