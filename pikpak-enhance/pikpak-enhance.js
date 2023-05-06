defc22a7b0231c8b68dc6c9baf38425f4d04d616
XiaoYingYo <XiaoYingYo@users.noreply.github.com>
2023-05-06 10:19:06 +0000

    update ↞ [auto-sync from `XiaoYingYo/MonkeyUserScripts`]

--- pikpak-enhance/pikpak-enhance.js
+++ pikpak-enhance/pikpak-enhance.js
@@ -19,7 +19,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://mypikpak.com/drive/*
 // @grant       none
-// @version     XiaoYing_2023.05.25.9
+// @version     XiaoYing_2023.05.25.10
 // @grant       GM_info
 // @grant       GM_getValue
 // @grant       GM_setValue
