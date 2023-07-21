f8a9b49fc04ce898f6056c204b41aed8a503e35a
Adam Lui 刘展鹏 <adamlui@pm.me>
2023-07-21 01:42:16 -0700

    Updated namespace meta, replaced string matching of regex...
    ...w/ faster `test` method
    
    ± Replaced `var autoSubmit` w/ `const autoSubmit`

--- mturk/mturk-a9-data-validation-hotkeys.user.js
+++ mturk/mturk-a9-data-validation-hotkeys.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          MTurk A9 Data Validation Hotkeys
-// @namespace     https://mturkers.org/tools/mturk-a9-data-validation-hotkeys/
-// @version       2017.05.16
+// @namespace     https://github.com/adamlui
+// @version       2023.7.21
 // @description   Hotkeys for Amazon's A9 Data Validation HITs on MTurk
-// @author        adaaaam
+// @author        Adam Lui
 // @include       https://*.mturk.com/*
 // @include       https://*.amazonaws.com/*
 // @require       http://code.jquery.com/jquery-latest.min.js
@@ -27,26 +27,26 @@ B - Top/Side Obscured
 
 E or Enter - Submit HIT
 
-NOTE: To activate auto-submission, replace the third line after this with "var autoSubmit = true;"
+NOTE: To activate auto-submission, replace the third line after this with "const autoSubmit = true;"
 */
 
-var autoSubmit = false;
+const autoSubmit = false;
 
 if (document.querySelector('iframe')) document.querySelector('iframe').focus();
 if ($('center:contains(a scale of 1 to 5, rate)').length) {
     window.addEventListener(`keydown`, function(event) {
         const mkey = event.key;
-        if (mkey.match(/[asdfgzxcvb1-5]/)) {
+        if (/[asdfgzxcvb1-5]/.test(mkey)) {
             if (mkey.match('z')) document.querySelectorAll(`[type='radio']`)[0].click();
             else if (mkey.match('x')) document.querySelectorAll(`[type='radio']`)[1].click();
             else if (mkey.match('c')) document.querySelectorAll(`[type='radio']`)[2].click();
             else if (mkey.match('v')) document.querySelectorAll(`[type='radio']`)[3].click();
             else if (mkey.match('b')) document.querySelectorAll(`[type='radio']`)[4].click();
-            else if (mkey.match(/[a1]/)) document.querySelector(`[type='radio'][value='1']`).click();
-            else if (mkey.match(/[s2]/)) document.querySelector(`[type='radio'][value='2']`).click();
-            else if (mkey.match(/[d3]/)) document.querySelector(`[type='radio'][value='3']`).click();
-            else if (mkey.match(/[f4]/)) document.querySelector(`[type='radio'][value='4']`).click();
-            else if (mkey.match(/[g5]/)) document.querySelector(`[type='radio'][value='5']`).click();
+            else if (/[a1]/.test(mkey)) document.querySelector(`[type='radio'][value='1']`).click();
+            else if (/[s2]/.test(mkey)) document.querySelector(`[type='radio'][value='2']`).click();
+            else if (/[d3]/.test(match)) document.querySelector(`[type='radio'][value='3']`).click();
+            else if (/[f4]/.test(mkey)) document.querySelector(`[type='radio'][value='4']`).click();
+            else if (/[g5]/.test(mkey)) document.querySelector(`[type='radio'][value='5']`).click();
             if (autoSubmit) document.querySelector(`[type='submit']`).click();
         }
         if (mkey.match('e')) document.querySelector(`[type='submit']`).click();
