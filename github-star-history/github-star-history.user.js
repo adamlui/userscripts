f415d48da76f58e03540f16f8003f5dc4aa3f035
Adam Lui <adamlui@pm.me>
2023-08-18 04:57:05 -0700

    Added ID check to avoid multi-insertions, refactored code
    ± Added/removed/re-ordered line breaks for readability

--- github-star-history/github-star-history.user.js
+++ github-star-history/github-star-history.user.js
@@ -3,7 +3,7 @@
 // @description         Adds star history graph to sidebar of GitHub repos
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.8.18.1
+// @version             2023.8.18.2
 // @license             MIT
 // @icon                https://github.githubassets.com/favicons/favicon.png
 // @match               *://github.com/*
@@ -28,27 +28,30 @@
     async function insertStarHistory() {
         const user = /github\.com\/(.*?)\//.exec(window.location)[1],
               repo = /.*\/(.*)/.exec(window.location)[1]
+        if (!document.querySelector('#star-history')) {
 
-        try { // to load/insert star history chart
-            const imgURL = `https://api.star-history.com/svg?repos=${ user }/${ repo }&type=Date`,
-                  img = await loadImg(imgURL)
+            try { // to load/insert star history chart
+                const imgURL = 'https://api.star-history.com/svg?repos='
+                             + `${ user }/${ repo }&type=Date`,
+                      img = await loadImg(imgURL)
 
-            if (img.complete && img.naturalHeight !== 0) {
+                if (img.complete && img.naturalHeight !== 0) {
 
-                // Create div + add attrs/HTML/listener
-                const starHistoryDiv = document.createElement('div')
-                starHistoryDiv.id = 'star-history'
-                starHistoryDiv.style.cursor = 'pointer'
-                starHistoryDiv.innerHTML = '<img style="width: 100% ; padding: 20px 0" '
-                    + 'src="' + imgURL + '">'
-                starHistoryDiv.addEventListener('click', () => { zoomStarHistory(imgURL) })
+                    // Create div + add attrs/HTML/listener
+                    const starHistoryDiv = document.createElement('div')
+                    starHistoryDiv.id = 'star-history'
+                    starHistoryDiv.style.cursor = 'pointer'
+                    starHistoryDiv.innerHTML = '<img style="width: 100% ; padding: 20px 0" '
+                        + 'src="' + imgURL + '">'
+                    starHistoryDiv.addEventListener('click', () => { zoomStarHistory(imgURL) })
 
-                // Insert div
-                const aboutSection = document.querySelector('[class$="sidebar"] > div > div')
-                aboutSection.insertAdjacentElement('afterend', starHistoryDiv)
-            }
+                    // Insert div
+                    const aboutSection = document.querySelector('[class$="sidebar"] > div > div')
+                    aboutSection.insertAdjacentElement('afterend', starHistoryDiv)
+                }
 
-        } catch (err) { console.error('Error loading star history chart:', err) }
+            } catch (err) { console.error('>> Error loading star history chart:', err) }
+        }
     }
 
     function loadImg(url) {
@@ -66,24 +69,17 @@
 
         // Create/stylize overlay
         const overlay = document.createElement('div')
-        overlay.style.position = 'fixed'
-        overlay.style.top = '0'
-        overlay.style.left = '0'
-        overlay.style.width = '100%'
-        overlay.style.height = '100%'
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'
-        overlay.style.display = 'flex'
-        overlay.style.alignItems = 'center'
-        overlay.style.justifyContent = 'center'
+        overlay.style.position = 'fixed' ; overlay.style.top = '0' ; overlay.style.left = '0'
+        overlay.style.width = '100%' ; overlay.style.height = '100%'
+        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)' ; overlay.style.display = 'flex'
+        overlay.style.alignItems = 'center' ; overlay.style.justifyContent = 'center'
         overlay.style.zIndex = '9999'
 
         // Stylize zoomed img
         const zoomedImg = new Image()
-        zoomedImg.style.maxWidth = '90%'
-        zoomedImg.style.maxHeight = '90%'
+        zoomedImg.title = 'View on star-history.com' ; zoomedImg.src = imgURL
+        zoomedImg.style.maxWidth = '90%' ; zoomedImg.style.maxHeight = '90%'
         zoomedImg.style.cursor = 'pointer'
-        zoomedImg.src = imgURL
-        zoomedImg.title = 'View on star-history.com'
 
         // Add listeners
         zoomedImg.addEventListener('click', () => { // view on star-history.com
