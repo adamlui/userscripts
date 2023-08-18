abbcc7b807388ca6cef3a6aff1b5457f1e478349
adamlui <adamlui@users.noreply.github.com>
2023-08-18 14:46:58 +0000

    GH sync test ↞ [auto-sync from `adamlui/github-star-history`]

--- github-star-history/github-star-history.user.js
+++ github-star-history/github-star-history.user.js
@@ -13,7 +13,7 @@
 // @description:zh-TW   將明星曆史圖表添加到 GitHub 存儲庫的側邊欄
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.8.18.6
+// @version             2023.8.18.7
 // @license             MIT
 // @icon                https://github.githubassets.com/favicons/favicon.png
 // @match               *://github.com/*
