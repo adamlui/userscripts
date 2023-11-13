04b0be1dd683508ba80472dd2ee5037649a0536b
Adam Lui 刘展鹏 <adamlui@pm.me>
2023-11-12 23:49:35 -0800

    Updated download/update URLs, added homepage/support URLs

--- hide-forum-images/hide-forum-images.user.js
+++ hide-forum-images/hide-forum-images.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             Hide Forum Images
-// @version          2023.11.12
+// @version          2023.11.12.1
 // @author           Adam Lui
 // @namespace        https://github.com/adamlui
 // @description      Hides images/videos from XenForo, vBulletin & Discourse forums.
@@ -13,8 +13,10 @@
 // @compatible       safari
 // @compatible       edge
 // @match            *://*/*
-// @downloadURL      https://update.greasyfork.org/scripts/12639.user.js
-// @updateURL        https://update.greasyfork.org/scripts/12639.meta.js
+// @downloadURL      https://update.greasyfork.org/scripts/12639/hide-forum-images.user.js
+// @updateURL        https://update.greasyfork.org/scripts/12639/hide-forum-images.meta.js
+// @homepageURL      https://github.com/adamlui/userscripts
+// @supportURL       https://github.com/adamlui/userscripts/issues
 // ==/UserScript==
 
 if (document.querySelector('[src*="vbulletin"], [src*="discourse"]') || /xenforo/i.test(document.querySelector('.copyright').textContent)) {
