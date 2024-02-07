1b8b31d5ff5f48813c59a5c10b6dcae3f844d5bd
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-07 14:07:07 -0800

    Added `'ignorePatterns': ['!/.github'],` to cover YAMLs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     'extends': ['eslint:recommended'],
+    'ignorePatterns': ['!/.github'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }], // enforce single quotes for string literals
