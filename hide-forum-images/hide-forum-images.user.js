d3ac4df77ee1a7dcebdb8cbbc496dc3f1c237cfa
Adam Lui 刘展鹏 <adamlui@pm.me>
2024-02-08 09:29:35 -0800

    Converted unnecessary template literal into string literal

--- hide-forum-images/hide-forum-images.user.js
+++ hide-forum-images/hide-forum-images.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Hide Forum Images
-// @version       2024.2.5
+// @version       2024.2.8
 // @author        Adam Lui
 // @namespace     https://adamlui.com
 // @description   Hides images/videos from XenForo, vBulletin & Discourse forums.
@@ -19,7 +19,7 @@
 // ==/UserScript==
 
 if (document.querySelector('[src*="vbulletin"], [src*="discourse"]') || /xenforo/i.test(document.querySelector('.copyright').textContent)) {
-    const css = `img, [style*="background-image"], [class*="avatar"], [class*="player"] { display:none !important; }`
+    const css = 'img, [style*="background-image"], [class*="avatar"], [class*="player"] { display:none !important; }'
     const styleNode = document.createElement('style') ; styleNode.innerText = css
     document.head.appendChild(styleNode)
 }
