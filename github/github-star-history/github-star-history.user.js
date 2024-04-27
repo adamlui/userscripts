01c17f98d507a14da6a3fa2ce3a996712da7e5b8
adamlui <adamlui@users.noreply.github.com>
2024-04-27 15:02:01 +0000

    Padded object destructuring for readability ↞ [auto-sync from `adamlui/github-star-history`]

--- github/github-star-history/github-star-history.user.js
+++ github/github-star-history/github-star-history.user.js
@@ -13,7 +13,7 @@
 // @description:zh-TW   將明星曆史圖表添加到 GitHub 存儲庫的側邊欄
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.4.27.3
+// @version             2024.4.27.4
 // @license             MIT
 // @icon                https://github.githubassets.com/favicons/favicon.png
 // @compatible          chrome
@@ -381,7 +381,7 @@
     }
 
     async function insertStarHistory() {
-        const {user, repo} = getUserAndRepoOfCurrentPage()
+        const { user, repo } = getUserAndRepoOfCurrentPage()
 
         try { // to load/insert star history chart
 
@@ -421,7 +421,7 @@
     }
 
     function zoomStarHistory(imgURL) {
-        const {user, repo} = getUserAndRepoOfCurrentPage()
+        const { user, repo } = getUserAndRepoOfCurrentPage()
 
         // Create/stylize overlay
         const overlay = document.createElement('div')
