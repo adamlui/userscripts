bca91878ce433c564224e5e9a2ce158bffe0d918
adamlui <adamlui@users.noreply.github.com>
2024-06-03 18:27:42 +0000

    Reverted condesning of int'l msg load request since recursively called ↞ [auto-sync from `adamlui/chatgpt-apps/chatgpt-auto-refresh`]

--- chatgpt/chatgpt-auto-refresh/chatgpt-auto-refresh.user.js
+++ chatgpt/chatgpt-auto-refresh/chatgpt-auto-refresh.user.js
@@ -220,7 +220,7 @@
 // @description:zu      *NGOKUPHEPHA* susa ukusetha kabusha ingxoxo yemizuzu eyi-10 + amaphutha enethiwekhi ahlala njalo + Ukuhlolwa kwe-Cloudflare ku-ChatGPT.
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.6.3.1
+// @version             2024.6.3.3
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
