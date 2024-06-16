1d76d97c002912e15dc3d9eab5404ff510a82cc7
adamlui <adamlui@users.noreply.github.com>
2024-06-16 14:31:10 +0000

    Renamed 'knob' to 'thumb' in `moveKnob()` name + comments ↞ [auto-sync from `adamlui/chatgpt-apps/googlegpt`]

--- chatgpt/googlegpt/googlegpt.user.js
+++ chatgpt/googlegpt/googlegpt.user.js
@@ -156,7 +156,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.16.6
+// @version             2024.6.16.8
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -891,8 +891,8 @@
           + '#googlegpt .loading { padding-bottom: 15px ; color: #b6b8ba ; animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite }'
           + '#googlegpt.sidebar-free { margin-left: 60px ; height: fit-content }'
           + '#font-size-slider-track { width: 98% ; height: 10px ; margin: 15px auto ; background-color: #ccc }'
-          + '#font-size-slider-thumb { width: 11px ; height: 20px ; border-radius: 30% ; position: relative ; top: -5px ;'
-              + `background-color: ${ scheme == 'dark' ? 'white' : '#000' } ;`
+          + '#font-size-slider-thumb { width: 10px ; height: 26px ; border-radius: 30% ; position: relative ; top: -8.25px ;'
+              + `background-color: ${ scheme == 'dark' ? 'white' : '#4a4a4a' } ;`
               + 'cursor: grab ; cursor: -webkit-grab ; cursor: -moz-grab }'
           + '#font-size-slider-thumb:active { cursor: grabbing ; cursor: -webkit-grabbing ; cursor: -moz-grabbing }'
           + '.standby-btn { width: 100% ; padding: 11px 0 ; cursor: pointer ; margin-top: 20px ;'
@@ -1217,7 +1217,7 @@
 
         // Init slider
         let fontSizeSlider = document.getElementById('font-size-slider-track')
-        if (!fontSizeSlider) { // create/append container/knob
+        if (!fontSizeSlider) { // create/append container/thumb
 
             // Create/append slider elems
             fontSizeSlider = document.createElement('div') ; fontSizeSlider.id = 'font-size-slider-track'
@@ -1226,40 +1226,40 @@
             fontSizeSlider.append(sliderThumb)
             appDiv.insertBefore(fontSizeSlider, appDiv.querySelector('.btn-tooltip,' // desktop
                                                                    + 'pre')) // mobile
-            // Init knob pos
+            // Init thumb pos
             setTimeout(() => {
                 const sliderWidth = fontSizeSlider.offsetWidth - sliderThumb.offsetWidth,
                       iniLeft = (config.fontSize - config.minFontSize) / (config.maxFontSize - config.minFontSize) * sliderWidth
                 sliderThumb.style.left = iniLeft + 'px'
             }, fadeInDelay) // to ensure visibility for accurate dimension calcs
 
-            // Add event listeners for dragging knob
+            // Add event listeners for dragging thumb
             let isDragging = false, startX, startLeft
             sliderThumb.addEventListener(inputEvents.down, event => {
                 event.preventDefault() // prevent highlighting
                 isDragging = true ; startX = event.clientX ; startLeft = sliderThumb.offsetLeft
             })
             document.addEventListener(inputEvents.move, event => {
-                if (isDragging) moveKnob(startLeft + event.clientX - startX) })
+                if (isDragging) moveThumb(startLeft + event.clientX - startX) })
             document.addEventListener(inputEvents.up, () => isDragging = false)
 
-            // Add event listener for wheel-scrolling knob
+            // Add event listener for wheel-scrolling thumb
             if (!isMobile) fontSizeSlider.addEventListener('wheel', event => {
                 event.preventDefault()
-                moveKnob(sliderThumb.offsetLeft + ( event.deltaY < 0 ? hWheelDistance : -hWheelDistance ))
+                moveThumb(sliderThumb.offsetLeft + ( event.deltaY < 0 ? hWheelDistance : -hWheelDistance ))
             })
 
-            function moveKnob(newLeft) {
+            function moveThumb(newLeft) {
 
-                // Bound knob
+                // Bound thumb
                 const sliderWidth = fontSizeSlider.offsetWidth - sliderThumb.offsetWidth
                 if (newLeft < 0) newLeft = 0
                 if (newLeft > sliderWidth) newLeft = sliderWidth
 
-                // Move knob
+                // Move thumb
                 sliderThumb.style.left = newLeft + 'px'
 
-                // Adjust font size based on knob position
+                // Adjust font size based on thumb position
                 const fontSizePercent = newLeft / sliderWidth,
                       fontSize = config.minFontSize + fontSizePercent * (config.maxFontSize - config.minFontSize)
                 answerPre.style.fontSize = fontSize + 'px'
