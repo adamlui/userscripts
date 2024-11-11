1cd7de4ed89597e77feaf669f2d98f0fa6a8509c
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-11-11 05:28:38 -0800

    Removed semicolons

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import json from 'eslint-plugin-json';
+import js from '@eslint/js'
+import globals from 'globals'
+import json from 'eslint-plugin-json'
 
 export default [
     js.configs.recommended,
@@ -28,4 +28,4 @@ export default [
     },
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ...json.configs['recommended'] }
-];
+]
