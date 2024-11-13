52a39c70832d783f7f424081e28dda169224d0f2
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-11-12 18:14:10 -0800

    Allowed missing label refs in MD to overcome false positive bug eslint/markdown#294

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,8 @@ export default [
         rules: {
             ...markdown.configs.recommended[0].rules,
             'markdown/heading-increment': 'off', // allow headings to skip levels
-            'markdown/fenced-code-language': 'off' // allow code blocks w/ no language specified
+            'markdown/fenced-code-language': 'off', // allow code blocks w/ no language specified
+            'markdown/no-missing-label-refs': 'off' // allow missing label references
         }
     },
     { files: ['**/*.yaml, **/*.yml'], ...eslintPluginYml.configs['flat/standard'][1] }
