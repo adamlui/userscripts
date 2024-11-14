099b9aebdfffca9c0481a7fb048e87be0839401c
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-14 05:37:52 -0800

    Moved `hljs` in JS config `languageOptions.globals` to start of line for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,8 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey,
-                chatgpt: 'readonly', CryptoJS: 'readonly', dom: 'readonly', GM_cookie: 'readonly', hljs: 'readonly',
-                ipv4: 'readonly', marked: 'readonly', renderMathInElement: 'readonly'
+                chatgpt: 'readonly', CryptoJS: 'readonly', dom: 'readonly', GM_cookie: 'readonly',
+                hljs: 'readonly', ipv4: 'readonly', marked: 'readonly', renderMathInElement: 'readonly'
             }
         },
         plugins: { regexp },
