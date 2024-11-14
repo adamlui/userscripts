2803b66df61bddbb6c4865c2133fbd4ba98f2235
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-14 05:24:13 -0800

    Moved `...js.configs.recommended.rules` inside JS config `rules` for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,9 @@ import eslintPluginYml from 'eslint-plugin-yml'
 
 export default [
     {
-        files: ['**/*.js', '**/*.mjs'], ...js.configs.recommended,
+        files: ['**/*.js', '**/*.mjs'],
         rules: {
+            ...js.configs.recommended.rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
