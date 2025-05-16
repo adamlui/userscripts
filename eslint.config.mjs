726c46c2da4755baeb3fe95d7682c14ccea5adae
Adam Lui <adam@kudoai.com>
2025-05-16 01:52:54 -0700

    Updated globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,11 +18,12 @@ export default [
                 chatbar: 'writable', chatgpt: 'readonly', checkBtnsToClick: 'readonly', config: 'writable',
                 CryptoJS: 'readonly', cryptoUtils: 'readonly', dom: 'readonly', env: 'writable', feedback: 'readonly',
                 GM_cookie: 'readonly', hljs: 'readonly', icons: 'writable', infinity: 'readonly', ipv4: 'readonly',
-                JSON5: 'readonly', log: 'readonly', marked: 'readonly', modals: 'writable', notify: 'readonly',
-                renderMathInElement: 'readonly', session: 'readonly', settings: 'writable', sites: 'writable',
+                JSON5: 'readonly', log: 'readonly', marked: 'readonly', modals: 'writable', msgChain: 'writable', notify: 'readonly',
+                prompts: 'readonly', renderMathInElement: 'readonly', replyBubble: 'writable',
+                session: 'readonly', settings: 'writable', show: 'readonly', sites: 'writable',
                 styles: 'writable', sync: 'readonly', syncConfigToUI: 'readonly', toggle: 'readonly',
                 toggleMode: 'readonly', toggles: 'writable', toolbarMenu: 'writable', tooltip: 'writable',
-                ui: 'readonly', updateCheck: 'readonly', webkitAudioContext: 'readonly'
+                ui: 'readonly', updateCheck: 'readonly', webkitAudioContext: 'readonly', xhr: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
