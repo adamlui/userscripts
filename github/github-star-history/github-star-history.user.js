99a7af3fdcaa7521bbed84d31230270c19873338
Adam Lui <adam@kudoai.com>
2025-07-28 01:15:09 -0700

    Utilized `isDarkMode()` in `ghAlert()`

--- github/github-star-history/github-star-history.user.js
+++ github/github-star-history/github-star-history.user.js
@@ -13,7 +13,7 @@
 // @description:zh-TW   將明星曆史圖表添加到 GitHub 存儲庫的側邊欄
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2025.7.28
+// @version             2025.7.28.1
 // @license             MIT
 // @icon                https://github.githubassets.com/favicons/favicon.png
 // @compatible          chrome
@@ -69,7 +69,7 @@
     // * Spaces are inserted into button labels by parsing function names in camel/kebab/snake case
 
         // Init env context
-        const scheme = window.matchMedia?.('(prefers-color-scheme: dark)')?.matches ? 'dark' : 'light',
+        const scheme = isDarkMode() ? 'dark' : 'light',
               isMobile = env.browser.isMobile
 
         // Define event handlers
@@ -298,7 +298,7 @@
         const closeSVGpath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
         closeSVGpath.setAttribute('fill-rule', 'evenodd')
         closeSVGpath.setAttribute('clip-rule', 'evenodd')
-        closeSVGpath.setAttribute('fill', window.matchMedia?.('(prefers-color-scheme: dark)')?.matches ? 'white' : 'black')
+        closeSVGpath.setAttribute('fill', isDarkMode() ? 'white' : 'black')
         closeSVGpath.setAttribute('d', 'M13.7071 1.70711C14.0976 1.31658 14.0976 0.683417 13.7071 0.292893C13.3166 -0.0976312 12.6834 -0.0976312 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976312 0.683417 -0.0976312 0.292893 0.292893C-0.0976312 0.683417 -0.0976312 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976312 12.6834 -0.0976312 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711Z')
         closeSVG.append(closeSVGpath) ; closeBtn.append(closeSVG)
 
