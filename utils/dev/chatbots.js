d3e110b35c04b2aa1f4fd2f616f95bf79d8be322
Adam Lui <adam@kudoai.com>
2026-06-27 04:02:22 -0700

    Re-ordered/grouped const inits

--- utils/dev/chatbots.js
+++ utils/dev/chatbots.js
@@ -5,17 +5,14 @@
 (() => {
     'use strict'
 
-    const chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt']
-
-    // Import LIBS
-    const { resolve, dirname } = require('path'),
+    const { dirname, resolve } = require('path'),
             spawn = require('cross-spawn')
 
-    // Init UI COLORS
-    const br = '\x1b[91m', // bright red
+    const chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt'],
+          br = '\x1b[91m', // bright red
           nc = '\x1b[0m'   // no color
 
-    // Init PATHS
+    // Init paths
     const repoRoot = (dir => {
         while (dir != '/' && !require('fs').existsSync(resolve(dir, 'package.json'))) dir = dirname(dir) ; return dir
     })(__dirname)
@@ -23,7 +20,7 @@
         chatbot => resolve(repoRoot, `chatgpt/${chatbot}/${chatbot}.user.js`)
     ).filter(path => require('fs').existsSync(path))
 
-    // OPEN files
+    // Open files
     spawn('code', ['-r', repoRoot, ...filePaths], { stdio: 'inherit' })
         .on('error', err => console.error(`${br}Failed to open VS Code: ${err.message}${nc}`))
 })()
