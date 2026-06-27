dfa48c239264c67b2eaf522dca4c5fedf502285d
Adam Lui <adam@kudoai.com>
2026-06-27 04:22:15 -0700

    Rid color vars, re-ordered inits

--- utils/dev/chatbots.js
+++ utils/dev/chatbots.js
@@ -5,17 +5,16 @@
 'use strict'
 
 (() => {
-    const { dirname, resolve } = require('path'),
-            fs = require('fs'),
-            spawn = require('cross-spawn'),
-            chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt'],
-            br = '\x1b[91m', // bright red
-            nc = '\x1b[0m'   // no color
+    const chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt'],
+        { dirname, resolve } = require('path'),
+          fs = require('fs'),
+          spawn = require('cross-spawn')
     const repoRoot = (dir => {
         while (dir != '/' && !fs.existsSync(resolve(dir, 'package.json'))) dir = dirname(dir) ; return dir })(__dirname)
     const filePaths = chatbots
         .map(chatbot => resolve(repoRoot, `chatgpt/${chatbot}/${chatbot}.user.js`))
         .filter(path => fs.existsSync(path))
+
     spawn('code', ['-r', repoRoot, ...filePaths], { stdio: 'inherit' })
-        .on('error', err => console.error(`${br}Failed to open VS Code: ${err.message}${nc}`))
+        .on('error', err => console.error(`Failed to open VS Code: ${err.message}`))
 })()
