60c60632086c2ffd38869f792b4772f02f4d8f0f
Adam Lui <adam@kudoai.com>
2026-06-27 04:04:11 -0700

    Condensed script

--- utils/dev/chatbots.js
+++ utils/dev/chatbots.js
@@ -6,21 +6,15 @@
     'use strict'
 
     const { dirname, resolve } = require('path'),
-            spawn = require('cross-spawn')
-
-    const chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt'],
-          br = '\x1b[91m', // bright red
-          nc = '\x1b[0m'   // no color
-
-    // Init paths
+            spawn = require('cross-spawn'),
+            chatbots = ['amazongpt', 'bravegpt', 'duckduckgpt', 'googlegpt'],
+            br = '\x1b[91m', // bright red
+            nc = '\x1b[0m'   // no color
     const repoRoot = (dir => {
         while (dir != '/' && !require('fs').existsSync(resolve(dir, 'package.json'))) dir = dirname(dir) ; return dir
     })(__dirname)
-    const filePaths = chatbots.map(
-        chatbot => resolve(repoRoot, `chatgpt/${chatbot}/${chatbot}.user.js`)
-    ).filter(path => require('fs').existsSync(path))
-
-    // Open files
+    const filePaths = chatbots.map(chatbot => resolve(repoRoot, `chatgpt/${chatbot}/${chatbot}.user.js`))
+        .filter(path => require('fs').existsSync(path))
     spawn('code', ['-r', repoRoot, ...filePaths], { stdio: 'inherit' })
         .on('error', err => console.error(`${br}Failed to open VS Code: ${err.message}${nc}`))
 })()
